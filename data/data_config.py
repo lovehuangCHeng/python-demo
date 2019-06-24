e28268e0d4263783f992da806e2e9952c831525a
lovehuangCHeng <527950770@qq.com>
2019-06-24 21:44:41 +0800

    基础数据

--- data/data_config.py
+++ data/data_config.py
@@ -1,16 +1,16 @@
 #coding:utf-8
 class global_var:
-     Id=0
-     rul=1
-     run=2
-     request_way=3
-     header=4
-     case_depend=5
-     data_depend=6
-     field_depend=7
-     data=8
-     expect=9
-     result=10
+     Id=0 # excel 模板中第一列编号：用例编号
+     rul=1 # excel 模板中第二列编号 ：接口地址
+     run=2 # excel 模板中第3列编号 ：测试接口是否执行
+     request_way=3 # excel 模板中第4列编号：请求方式
+     header=4 # excel 模板中第5列编号 ：是否含有header
+     case_depend=5 # excel 模板中第6列编号：依赖caseid
+     data_depend=6 # excel 模板中第7列编号 ：依赖的返回数据
+     field_depend=7 # excel 模板中第8列编号 ：数据依赖字段
+     data=8 # excel 模板中第9列编号 ：请求数据
+     expect=9 # excel 模板中第10列编号：期望结果
+     result=10 # excel 模板中第11列编号 ：实际结果
 #获取caseID
 def get_id():
      return  global_var.Id
